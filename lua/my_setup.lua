1c3449e9082f19cd9fdc8630066e6f010a9c57da
Anthony Tornetta <cornchipgonecodin@gmail.com>
2025-08-11 12:20:35 -0400

    Custom remaps

--- lua/my_setup.lua
+++ lua/my_setup.lua
@@ -38,3 +38,6 @@
 
 vim.cmd "noremap k j|noremap <C-w>k <C-w>j|noremap <C-w><C-k> <C-w>j"
 vim.cmd "noremap j k|noremap <C-w>j <C-w>k|noremap <C-w><C-j> <C-w>k"
+vim.cmd 'nnoremap f "_d'
+
+-- require("copilot.suggestion").toggle_auto_trigger()
