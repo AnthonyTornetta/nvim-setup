77aeaf609500479bb3e2af6e22f3e399434e1310
Anthony Tornetta <cornchipgonecodin@gmail.com>
2025-08-13 23:06:27 -0400

    Enable inlay hints + git blame inline

--- lua/polish.lua
+++ lua/polish.lua
@@ -2,3 +2,5 @@ vim.cmd "noremap k j|noremap <C-w>k <C-w>j|noremap <C-w><C-k> <C-w>j"
 vim.cmd "noremap j k|noremap <C-w>j <C-w>k|noremap <C-w><C-j> <C-w>k"
 vim.cmd 'nnoremap f "_d'
 
+require("gitsigns").toggle_current_line_blame(true)
+
