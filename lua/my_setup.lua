7f666f2854de4759e95a8b603947e1d711ead6e7
AnthonyTornetta <cornchipgonecodin@gmail.com>
2025-09-27 01:22:04 -0400

    Updating plugins + adding open in gh plugin

--- lua/my_setup.lua
+++ lua/my_setup.lua
@@ -40,4 +40,6 @@ vim.cmd "noremap k j|noremap <C-w>k <C-w>j|noremap <C-w><C-k> <C-w>j"
 vim.cmd "noremap j k|noremap <C-w>j <C-w>k|noremap <C-w><C-j> <C-w>k"
 vim.cmd 'nnoremap f "_d'
 
+vim.api.nvim_set_keymap("n", "<Leader>go", ":OpenInGHFileLines <CR>", { silent = true, noremap = true })
+
 -- require("copilot.suggestion").toggle_auto_trigger()
