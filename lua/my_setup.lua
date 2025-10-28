5282d83df0aad31315ac5dfceffee66270ce328f
Anthony Tornetta <cornchipgonecodin@gmail.com>
2025-10-28 01:24:32 -0400

    Adding grug-far and fixing yaml

--- lua/my_setup.lua
+++ lua/my_setup.lua
@@ -40,6 +40,8 @@ vim.cmd "noremap k j|noremap <C-w>k <C-w>j|noremap <C-w><C-k> <C-w>j"
 vim.cmd "noremap j k|noremap <C-w>j <C-w>k|noremap <C-w><C-j> <C-w>k"
 vim.cmd 'nnoremap f "_d'
 
-vim.api.nvim_set_keymap("n", "<Leader>go", ":OpenInGHFileLines <CR>", { silent = true, noremap = true })
+vim.api.nvim_set_keymap("n", "<Leader>gh", ":OpenInGHFileLines <CR>", { silent = true, noremap = true })
+vim.api.nvim_set_keymap("n", "<Leader>r", ":GrugFar <CR>", { silent = true, noremap = true })
+vim.api.nvim_set_keymap("v", "<Leader>r", ":GrugFarWithin <CR>", { silent = true, noremap = true })
 
 -- require("copilot.suggestion").toggle_auto_trigger()
--- lua/plugins/far.lua
+++ lua/plugins/far.lua
@@ -1,4 +1,13 @@
 ---@type LazySpec
 return {
-  "brooth/far.vim",
+  "MagicDuck/grug-far.nvim",
+  config = function()
+    -- optional setup call to override plugin options
+    -- alternatively you can set options with vim.g.grug_far = { ... }
+    require("grug-far").setup {
+
+      -- options, see Configuration section below
+      -- there are no required options atm
+    }
+  end,
 }
