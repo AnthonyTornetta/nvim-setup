787ee6a8553c3e8a32fb1a804822371214b0e045
Anthony Tornetta <cornchipgonecodin@gmail.com>
2026-06-11 14:42:44 -0400

    Update to Astro v6

--- lua/plugins/dap-ui.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "williamboman/mason.nvim",
-  "mfussenegger/nvim-dap",
-  "jay-babu/mason-nvim-dap.nvim",
-  {
-    "rcarriga/nvim-dap-ui",
-    dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
-  }
-}
